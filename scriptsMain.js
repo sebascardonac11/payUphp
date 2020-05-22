72c3f979a5f39e784fe75cbae96a33a892235773
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2020-05-21 21:32:50 -0500

    Tercera ida a PayU y ver el error

--- scriptsMain.js
+++ scriptsMain.js
@@ -4,11 +4,17 @@
 $(document).ready(function(){
 
  $(".test").click(function( event ) {
-		alert("Probando test");
-	  console.log( "clicked: " + event.target);
-	  
 	  $.ajax({url: "https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi", 
 		  	method: "POST",
+		  	headers:{ 
+		  		 "test": false,
+		  		 "language": "en",
+		  		 "command": "PING",
+		  		 "merchant": {
+		  	     "apiLogin": "pRRXKOl8ikMmt9u",
+		  	     "apiKey": "4Vj8eK4rloUd272L48hsrarnUA"
+		  		 }
+		  	 },
 		  	 data: { 
 		  		 "test": false,
 		  		 "language": "en",
@@ -25,8 +31,8 @@ $(document).ready(function(){
 		  	 }).done(function() {
 		  	    alert( "success" );
 		  	  })
-		  	  .fail(function() {
-		  	    alert( "error" );
+		  	  .fail(function(res) {
+		  	    alert( "error"+res );
 		  	  });
 	  
 	});
