152afecd23a83079df984053dd00d2df506b0326
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2020-05-21 21:46:23 -0500

    Sin header

--- scriptsMain.js
+++ scriptsMain.js
@@ -6,15 +6,7 @@ $(document).ready(function(){
  $(".test").click(function( event ) {
 	  $.ajax({url: "https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi", 
 		  	method: "POST",
-		  	headers:{ 
-		  		 "test": false,
-		  		 "language": "en",
-		  		 "command": "PING",
-		  		 "merchant": {
-		  	     "apiLogin": "pRRXKOl8ikMmt9u",
-		  	     "apiKey": "4Vj8eK4rloUd272L48hsrarnUA"
-		  		 }
-		  	 },
+		  	headers:null,
 		  	 data: { 
 		  		 "test": false,
 		  		 "language": "en",
