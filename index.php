65a61901e7a87e703d3dbd592e41ce55e60e7706
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2020-05-21 20:00:12 -0500

    Subiendo ScriptMain2.js

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <head>
         <title>sebascardonac11!!!</title>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="scriptsMain.js"></script>
     </head>
     <body>
         <h1>PAYU con PHP!!!!</h1>
