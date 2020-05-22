637ff0eb31f256141f7f8addc96e04db1ce236c5
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2020-05-21 21:14:20 -0500

    Primer ida a PayU

--- scriptsMain.js
+++ scriptsMain.js
@@ -6,16 +6,25 @@ $(document).ready(function(){
 
  $(".test").click(function( event ) {
 		alert("Probando test");
-	  console.log( "clicked: " );
+	  console.log( "clicked: " + event.target);
+	  
+	  $.ajax({url: "https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi", 
+		  	method: "POST",
+		  	 data: { 
+		  		 "test": false,
+		  		 "language": "en",
+		  		 "command": "PING",
+		  		 "merchant": {
+		  	     "apiLogin": "pRRXKOl8ikMmt9u",
+		  	     "apiKey": "4Vj8eK4rloUd272L48hsrarnUA"
+		  		 }
+		  	 }
+		  	success: function(result){
+		    $("#msgid").html(result);
+		  }});
+	  
 	});
-	$(".test2").click(function() {
-		  alert("Probando test2 ");
-		  console.log( "clicked: "  );
-		});
 
-	$( "#target" ).click(function() {
-		  alert( "Handler for .click() called." );
-		});
 
 });
 
