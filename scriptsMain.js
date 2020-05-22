ec6a6570fd3f614270a1629fe687f1aa377032bd
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2020-05-21 21:56:36 -0500

    datatype add

--- scriptsMain.js
+++ scriptsMain.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
  $(".test").click(function( event ) {
 	  $.ajax({url: "https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi", 
 		  	method: "POST",
-		  	type: 'POST',
-            dataType: 'json',
+		  	type: 'json',
 		  	headers:null,
 		  	 data: { 
 		  		 "test": false,
@@ -21,13 +20,12 @@ $(document).ready(function(){
 		  	success: function(result){
 		    $("#msgid").html(result);
 		    
-		  }
-		  	 }).done(function() {
-		  	    alert( "success" );
-		  	  })
-		  	  .fail(function(jqXHR, textStatus) {
-		  	    alert( "error: "+textStatus );
-		  	  });
+		  },
+	        error: function (error) {
+	        	  $("#msgid").html(result);
+	            console.log(error);
+	        }
+		  	 })
 	  
 	});
 
