766f304369567cf73a17b0907aaa67d199526bbd
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2020-05-21 20:42:16 -0500

    Solucionando onclick

--- scriptsMain.js
+++ scriptsMain.js
@@ -3,17 +3,19 @@
  */
 $(document).ready(function(){
  $("#msgid").html("This is Hello World by JQuery");
+
+ $(".test").click(function( event ) {
+		alert("Probando test");
+	  console.log( "clicked: " );
+	});
+	$(".test2").click(function() {
+		  alert("Probando test2 ");
+		  console.log( "clicked: "  );
+		});
+
+	$( "#target" ).click(function() {
+		  alert( "Handler for .click() called." );
+		});
+
 });
 
-$(".test").click(function( event ) {
-	alert("Probando test");
-  console.log( "clicked: " );
-});
-$(".test2").click(function() {
-	  alert("Probando test2 ");
-	  console.log( "clicked: "  );
-	});
-
-$( "#target" ).click(function() {
-	  alert( "Handler for .click() called." );
-	});
